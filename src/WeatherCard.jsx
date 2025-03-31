910fcb67d42c441566d2e50e473a7ce8cb8626b1
AmritShetra <33128635+AmritShetra@users.noreply.github.com>
2025-03-31 20:47:31 +0100

    Cleaned up WeatherCard function

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -4,11 +4,26 @@ import { convertTimestamp, convertWeathercode, convertToF } from './conversion';
 
 export default function WeatherCard(props) {
 
+    // If set to Celsius, return temp - if not, convert to Fahrenheit
+    function getTemp() {
+        return props.scale === '°C' ? props.temp : convertToF(props.temp);
+    }
+
+    // Time
+    // Weather icon with time
+    // Temperature and clickable scale
+    // Weathercode, i.e. "Clear sky"
     return (
         <div id='weather-card'>
             <h3>{convertTimestamp(props.time)}</h3>
-            <WeatherIcon weathercode={props.weathercode} time={props.time} sunrise={props.sunrise} sunset={props.sunset} />
-            <h2>{props.scale ==='°C'? props.temp : convertToF(props.temp)}<span onClick={props.changeScale}>{props.scale}</span></h2>
+            <WeatherIcon 
+                weathercode={props.weathercode} 
+                time={props.time} 
+                sunrise={props.sunrise} 
+                sunset={props.sunset} 
+            />
+            <h2>
+                {getTemp()}<span onClick={props.changeScale}>{props.scale}</span></h2>
             <span>{convertWeathercode(props.weathercode)}</span>
         </div>
     );
